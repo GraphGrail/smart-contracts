9092e4c18e9b759607de50127a4bbb722219d8f4
Sam Kozin <sam.kozin@gmail.com>
2018-03-17 22:02:09 +0300

    Use abstract contract instead of interface to represent ERC20 token

--- truffle/contracts/ERC20.sol
+++ truffle/contracts/ERC20.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.17;
 
 
-interface ERC20 {
+contract ERC20 {
   function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);

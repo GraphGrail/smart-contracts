f43173bf88597a0ee973456ec94c92e1951de826
Sam Kozin <sam.kozin@gmail.com>
2018-03-20 16:23:57 +0300

    Add assert for testing BigNumber equality

--- truffle/test/helpers.js
+++ truffle/test/helpers.js
@@ -1,4 +1,6 @@
-const assert = require('chai').assert
+import chai from 'chai'
+
+const assert = chai.assert
 
 export async function assertRevert(promise, message) {
   try {
@@ -8,4 +10,8 @@ export async function assertRevert(promise, message) {
     const revertFound = error.message.search('revert') >= 0
     assert(revertFound, `Expected "revert", got ${error} instead ${message}`)
   }
+}
+
+assert.bignumEqual = function assertBignumEqual(bal1, bal2, message) {
+  assert.equal(bal1.toString(), bal2.toString(), message)
 }

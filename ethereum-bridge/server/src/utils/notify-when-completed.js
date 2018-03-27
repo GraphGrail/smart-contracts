cf8fcaf091b65279ee75ab6e2ace49a9afae84b8
ujenjt <pshenichniy.eugene@gmail.com>
2018-03-27 17:00:36 +0300

    Add logging for outcoming requests

--- ethereum-bridge/server/src/utils/notify-when-completed.js
+++ ethereum-bridge/server/src/utils/notify-when-completed.js
@@ -1,8 +1,47 @@
+import Pino from 'pino'
+const pino = Pino()
+
 import BigNumber from 'bignumber.js'
 import nanoid from 'nanoid'
-import needle from 'needle'
+
+import request from 'superagent'
+import verboseErrors from 'superagent-verbose-errors'
 
 BigNumber.config({EXPONENTIAL_AT: [-7, 30]})
+
+function requestLogger(taskId) {
+  return (request) => {
+    pino.info({taskId, request}, 'outgoing request')
+    return request
+  }
+}
+
+function logResponseData(resp, taskId) {
+  const logData = {
+    type: resp.type,
+    charset: resp.charset,
+    statusCode: resp.statusCode,
+    headers: resp.headers,
+    body: resp.body,
+    taskId: taskId,
+  }
+  pino.info(logData, 'response to outgoing request')
+}
+
+function logErrorData(err, taskId) {
+  const response = err.response
+  const logData = {
+    type: response.type,
+    charset: response.charset,
+    statusCode: response.statusCode,
+    headers: response.headers,
+    body: response.body,
+    text: response.text,
+    error: response.error,
+    taskId: taskId,
+  }
+  pino.error(logData, 'response to outgoing request failed')
+}
 
 export default function notifyWhenCompleted(httpCallback, promise) {
   const taskId = nanoid()
@@ -16,7 +55,7 @@ export default function notifyWhenCompleted(httpCallback, promise) {
     })
 
   const onError = err => {
-    console.log(err.stack)
+    pino.error({taskId, err}, 'task failed')
     return postToCallback(httpCallback, {
       taskId: taskId,
       success: false,
@@ -28,13 +67,23 @@ export default function notifyWhenCompleted(httpCallback, promise) {
     })
   }
 
-  promise.then(onSuccess).catch(onError).catch(err => {
-    console.error(`Failed to POST to callback ${httpCallback}: ${err.stack}`)
-  })
+  promise.then(onSuccess).catch(onError)
 
   return taskId
 }
 
 function postToCallback(httpCallback, body) {
-  return needle('post', httpCallback, body, {json: true})
+  const taskId = body.taskId
+  return request
+    .post(httpCallback)
+    .set('Content-Type', 'application/json')
+    .send(body)
+    .use(verboseErrors)
+    .use(requestLogger(body.taskId))
+    .then((resp) => {
+      logResponseData(resp, taskId)
+    })
+    .catch(err => {
+      logErrorData(err, taskId)
+    })
 }

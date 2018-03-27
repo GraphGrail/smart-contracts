e6ca27b518cf1e4006fe32313fb78bb33e020250
Sam Kozin <sam.kozin@gmail.com>
2018-03-27 15:55:56 +0300

    Fix warning in setWeb3Promise(); throw proper error on conn. failure

--- ethereum-bridge/shared/utils/connection.js
+++ ethereum-bridge/shared/utils/connection.js
@@ -1,18 +1,33 @@
+import {UserError} from '../errors'
+import * as ErrorCodes from '../error-codes'
+
 import {promisifyCall} from './promisify'
 
+
 let connectionPromise
+let connectionError
 
 export function setWeb3Promise(web3Promise) {
   connectionPromise = makeConnectionPromise(web3Promise)
+    .catch(err => {connectionError = err})
 }
 
-export function getConnection() {
+export async function getConnection() {
   if (!connectionPromise) {
     throw new Error(
       `you should set web3 Promise by calling setWeb3Promise before calling getConnection`
     )
   }
-  return connectionPromise
+  const connection = await connectionPromise
+  if (connectionError) {
+    throw (/Invalid JSON RPC response: ""/.test(connectionError.message)
+      ? new UserError('Failed to connect to Ethereum node',
+          ErrorCodes.NO_ETHEREUM_CLIENT,
+          connectionError)
+      : connectionError
+    )
+  }
+  return connection
 }
 
 async function makeConnectionPromise(web3Promise) {

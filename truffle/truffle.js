e137d28b6fb5ba5096ff0259892311ae4f59633a
Anton Kondakov <avkondakov@gmail.com>
2018-04-16 18:30:04 +0300

    Config for rinkeby deploy

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -5,7 +5,7 @@ const GWEI = 1000000000
 
 const RINKEBY_GAS_LIMIT = 6650000
 const RINKEBY_GAS_PRICE = 20 * GWEI
-const RINKEBY_DEPLOY_FROM = "0x6da26a02b4364dcff7cfd58f8a8b9c6ce62a0c61"
+const RINKEBY_DEPLOY_FROM = "0x389ED518dcaA84F55C74032e446205555cb94fC7"
 
 module.exports = {
   networks: {
@@ -18,7 +18,7 @@ module.exports = {
     },
     rinkeby: {
       host: 'localhost',
-      port: 9545,
+      port: 8545,
       network_id: 4,
       gas: RINKEBY_GAS_LIMIT,
       gasPrice: RINKEBY_GAS_PRICE,

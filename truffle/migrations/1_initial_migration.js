21cee1a587c45c31583926b61ecce2573263ee10
GraphGrail DevBot <graphgrail@graphgrail.com>
2018-04-17 22:57:06 +0300

    Migrate to rinkeby

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -1,5 +1,15 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const Web3 = require('web3');
 
-module.exports = function(deployer) {
+var Migrations = artifacts.require('./Migrations.sol');
+
+module.exports = function(deployer, network, addresses) {
+  if (process.env.ACCOUNT_PASSWORD) {
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://geth:8545'));
+
+    console.log('>> Unlocking account 0x389ED518dcaA84F55C74032e446205555cb94fC7');
+    web3.personal.unlockAccount('0x389ED518dcaA84F55C74032e446205555cb94fC7', process.env.ACCOUNT_PASSWORD);
+  }
+
+  console.log('>> Deploying migration');
   deployer.deploy(Migrations);
 };
--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,11 +1,11 @@
-require('babel-register')
+require('babel-register');
 
-const GAS_LIMIT = 4712388
-const GWEI = 1000000000
+const GAS_LIMIT = 4712388;
+const GWEI = 1000000000;
 
-const RINKEBY_GAS_LIMIT = 6650000
-const RINKEBY_GAS_PRICE = 20 * GWEI
-const RINKEBY_DEPLOY_FROM = "0x389ED518dcaA84F55C74032e446205555cb94fC7"
+const RINKEBY_GAS_LIMIT = 6650000;
+const RINKEBY_GAS_PRICE = 20 * GWEI;
+const RINKEBY_DEPLOY_FROM = '0x389ED518dcaA84F55C74032e446205555cb94fC7';
 
 module.exports = {
   networks: {
@@ -17,7 +17,7 @@ module.exports = {
       gasPrice: 1 * GWEI,
     },
     rinkeby: {
-      host: 'localhost',
+      host: 'geth',
       port: 8545,
       network_id: 4,
       gas: RINKEBY_GAS_LIMIT,
